43594d11ef9bf47848508ca4bff85bd190d75bf6
fabmaiadev <fabxmaia@outlook.com>
2022-12-18 16:18:07 -0300

    feature/main - Terceiro Commit - criado o metodo savePost criado botao do form e pagina para implementar form

--- src/main/java/com/spring/codeblogp/controller/PostController.java
+++ src/main/java/com/spring/codeblogp/controller/PostController.java
@@ -4,11 +4,15 @@ import com.spring.codeblogp.model.Post;
 import com.spring.codeblogp.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -37,4 +41,19 @@ public class PostController {
         return modelAndViewPost;
     }
 
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm() {
+
+        return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if(result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setDataDeCriacaoDoPost(LocalDate.now());
+        postService.save(post);
+        return "redirect:/posts";
+    }
 }
